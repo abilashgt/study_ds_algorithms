6071ba0d973f0cca60580798b651802f3197bcbe
Abilash George Thomas <abilash_thomas@apple.com>
2020-04-06 19:34:29 -0700

    tree serialize and deserialize

---
+++ basic_ds_algo_java/src/main/java/trees/TreeNode.java
@@ -0,0 +1,10 @@
+package trees;
+
+public class TreeNode {
+    public Integer val;
+    public TreeNode left, right;
+    public TreeNode(Integer val) {
+        this.val = val;
+    }
+
+}
---
+++ basic_ds_algo_java/src/main/java/trees/questions/SerializeDeserializeBinaryTree.java
@@ -0,0 +1,64 @@
+package trees.questions;
+
+import trees.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// public class Codec
+public class SerializeDeserializeBinaryTree {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        String result = preorderS(root, "");
+        System.out.println("final-"+result);
+        return result;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String arrStr[] = data.split(",");
+        Queue<String> queue = new LinkedList<String>();
+
+        for(int i=0; i<arrStr.length; i++){
+            queue.add(arrStr[i]);
+        }
+
+        // deserialize
+        TreeNode root = preorderD(queue);
+
+        return root;
+    }
+
+    // deserialise
+    static TreeNode preorderD(Queue<String> queue){
+        String item = queue.remove();
+
+        if(!item.equals("null")){
+            TreeNode root = new TreeNode(Integer.parseInt(item));
+            root.left = preorderD(queue);
+            root.right = preorderD(queue);
+            return root;
+        }
+
+        return null;
+    }
+
+    // serialization
+    static String preorderS(TreeNode root, String result){
+
+        if(result!=""){
+            result+=",";
+        }
+
+        if(root!=null){
+            result += root.val;
+            result = preorderS(root.left, result);
+            result = preorderS(root.right, result);
+        }
+        else{
+            result += "null";
+        }
+
+        return result;
+    }
+}
